b95573fd96c8804e7bf02bf47ccbe6a41ddeb934
sofiaamanda <127509439+sofiaamanda@users.noreply.github.com>
2023-03-30 11:45:29 +0800

    Add files via upload

---
+++ TestErr0152.java
@@ -0,0 +1,21 @@
+/*sofia amanda*/
+/*13020210152*/
+
+import java.util.Scanner;
+public class TestErr0152 {
+	/**
+	 * @param args
+	 */
+	public static void main (String[] args) {
+	//TODO Auto-generated method stub
+	/*kamus*/
+	Scanner input = new Scanner (System.in);
+	System.out.print ("masukan nilai : ");
+	int i = input.nextInt();
+	double k = 100;
+	char c = 's';
+	boolean falses = true;
+	int j = i+1;
+	System.out.println("j adalah "+ j + "k adalah "+k + "dan c adalah "+c + "dan trues adalah "+false);
+	}
+}
